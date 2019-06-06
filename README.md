72981454644627865e1376184c1b3f42a98cece4
itsfadnis <nik.fadnis@gmail.com>
2019-06-06 11:49:44 +0530

    Add datadog transport :rocket:

---
+++ README.md
@@ -0,0 +1,30 @@
+# datadog-winston
+Ship winston logs to datadog without breaking a sweat
+
+## Options
+- **apiKey**: Your datadog api key *[required]*
+- **hostname**: The machine/server hostname
+- **service**: The name of the application or service generating the logs
+- **ddsource**: The technology from which the logs originated
+- **ddtags**: Metadata assoicated with the logs
+
+## Usage
+```javascript
+var winston = require('winston')
+var DatadogWinston = require('datadog-winston')
+
+var logger = winston.createLogger({
+  // Whatever options you need
+  // Refer https://github.com/winstonjs/winston#creating-your-own-logger
+})
+
+logger.add(
+  new DatadogWinston({
+    apiKey: 'super_secret_datadog_api_key',
+    hostname: 'my_machine',
+    service: 'super_service',
+    ddsource: 'node.js',
+    ddtags: 'foo:bar,boo:baz'
+  })
+)
+```
---
+++ __tests__/index.js
@@ -0,0 +1,60 @@
+/* global describe, beforeEach, afterEach, jest, expect, it */
+
+const Transport = require('winston-transport')
+const DatadogTransport = require('../lib')
+const nock = require('nock')
+
+it('extends winston transport', () => {
+  expect(DatadogTransport.prototype).toBeInstanceOf(Transport)
+})
+
+it('throws an error if api key isn\'t passed in', () => {
+  try {
+    // eslint-disable-next-line
+    new DatadogTransport()
+  } catch (e) {
+    expect(e.message).toBe('Missing required option: `apiKey`')
+  }
+})
+
+describe('DatadogTransport#log(info, callback)', () => {
+  let scope
+
+  beforeEach(() => {
+    scope = nock('https://http-intake.logs.datadoghq.com', {
+      reqheaders: {
+        'content-type': 'application/json'
+      }
+    })
+
+    scope.post(
+      '/v1/inputapikey',
+      JSON.stringify({
+        foo: 'bar'
+      })
+    ).query({
+      service: 'service',
+      ddsource: 'ddsource',
+      ddtags: 'ddtags',
+      hostname: 'hostname'
+    }).reply(204)
+  })
+
+  afterEach(() => {
+    nock.restore()
+  })
+
+  it('transports logs to datadog', async () => {
+    const transport = new DatadogTransport({
+      apiKey: 'apikey',
+      service: 'service',
+      ddsource: 'ddsource',
+      ddtags: 'ddtags',
+      hostname: 'hostname'
+    })
+    const callback = jest.fn()
+    await transport.log({ foo: 'bar' }, callback)
+    expect(scope.isDone()).toBe(true)
+    expect(callback).toHaveBeenCalled()
+  })
+})
